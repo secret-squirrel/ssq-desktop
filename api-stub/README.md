9c85f555f74792f88de302534cc9fa97957b1efa
Matt MacAulay <matt@dulcepericulum.org>
2014-06-09 20:56:06 -0400

    Initial working gulpfile

--- api-stub/README.md
+++
@@ -1,2 +0,0 @@
-# API Stub Documentation
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,61 @@
 var gulp = require('gulp')
+var mocha = require('gulp-mocha')
+var gutil = require('gulp-util')
+var stylus = require('gulp-stylus')
 var shell = require('gulp-shell')
-var NwBuilder = require('node-webkit-builder')
+var preprocess = require('gulp-preprocess')
 
-var nw = new NwBuilder({
-  version: '0.8.6',
-  files: './src/**/**',
-  platforms: ['osx']
+var paths = {
+  scripts: ['src/js/**/*.js'],
+  styles: ['src/styles/**/*.styl'],
+  html: ['src/html/**/*.html'],
+  tests: ['test/**/*-test.js'],
+  vendor: ['vendor/**'],
+  other: ['src/package.json']
+}
+
+gulp.task('html', function() {
+  return gulp.src(paths.html)
+    .pipe(preprocess())
+    .pipe(gulp.dest('build'))
 })
 
-nw.on('log',  console.log)
-
-gulp.task('shell-install', shell.task([
-  'bower cache clean && bower install && cd src && npm install'
-]))
-
-gulp.task('node-webkit-install', function() {
-  return nw.build().then(function () {
-    console.log('Successfully built node-webkit.')
-  }).catch(function (error) {
-    console.error(error)
-  })
+gulp.task('scripts', function() {
+  return gulp.src(paths.scripts)
+    .pipe(preprocess())
+    .pipe(gulp.dest('build/js'))
 })
 
-gulp.task('install', ['shell-install', 'node-webkit-install'])
-
-gulp.task('run', function() {
-  return nw.run()
+gulp.task('styles', function() {
+  return gulp.src(paths.styles)
+    .pipe(stylus())
+    .pipe(preprocess())
+    .pipe(gulp.dest('build'))
 })
 
-gulp.task('default', ['run'])
+gulp.task('vendor', function() {
+  return gulp.src(paths.vendor)
+    .pipe(gulp.dest('build/vendor'))
+})
+
+gulp.task('other', function() {
+  return gulp.src(paths.other)
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('test', function() {
+  return gulp.src(['test/**/*.js'], { read: false })
+    .pipe(mocha({ reporter: 'list' }))
+    .on('error', gutil.log)
+})
+
+gulp.task('watch', function() {
+  gulp.watch(paths.html, ['html'])
+  gulp.watch(paths.scripts, ['scripts'])
+  gulp.watch(paths.vendor, ['vendor'])
+  gulp.watch(paths.other, ['other'])
+  gulp.watch(paths.styles, ['styles'])
+  gulp.watch(['src/**', 'test/**'], ['test'])
+})
+
+gulp.task('default', ['html', 'scripts', 'styles', 'vendor', 'other', 'test', 'watch'])
